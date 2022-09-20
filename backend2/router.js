de45a77d2f13acc275112a41f8bc3c366ebf82ed
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-09-20 12:32:06 -0700

    add

---
+++ backend2/router.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express().Router();
+
+router.get("/mood", (req, res) => {
+
+});
+
+router.get("/mood/:moodId", (req, res) => {
+
+});
+
+router.post("/mood", (req, res) => {
+
+});
+
+router.put("/mood/:moodId", (req, res) => {
+
+});
+
+router.delete("/mood/:moodId", (req, res) => {
+
+});
+
